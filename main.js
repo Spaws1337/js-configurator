786ab28eed159663b9c2c04c8a2d9685a233831a
Spaws1337 <Kerrymonich@yandex.ru>
2024-04-27 15:28:50 +0300

    Рабочий визуал

--- main.js
+++ main.js
@@ -1,33 +1,82 @@
 const fileInput = document.getElementById('fileInput');
-
-const jsonData = {
-  data: null
-};
+const jsonData = { data: null };
+import translit from "./translit.js";
 
 fileInput.addEventListener('change', (event) => {
   const file = event.target.files[0];
   if (file) {
     const reader = new FileReader();
     reader.onload = () => {
-      // Сохраняем распарсенные данные в jsonData.data
       jsonData.data = JSON.parse(reader.result);
-      console.log(jsonData.data); // Выводим данные в консоль
+      generatePage();
     };
     reader.readAsText(file);
   }
 });
 
-const loadButton = document.getElementById('loadButton');
+function generatePage() {
+  if (jsonData.data && jsonData.data.Devices && jsonData.data.Devices.length > 0) {
+    const device = jsonData.data.Devices[0];
+    const addDocument = document.querySelector('.configurator__item');
+    addDocument.innerHTML = '';
 
-function processJsonData() {
-  if (jsonData.data) {
-    // Здесь вы можете обрабатывать данные JSON
-    console.log('Обработка данных JSON:', jsonData.data);
-  } else {
-    console.log('Данные JSON еще не загружены');
+    if (device.Docs) {
+      device.Docs.forEach(doc => {
+        let templateDocsHTML = '';
+
+        function generateTemplateDocsHTML(templateDocs, parentName) {
+          if (Array.isArray(templateDocs)) {
+            templateDocs.forEach(doc => {
+              if (doc.Name) {
+                templateDocsHTML += `
+                  <div class="custom-checkbox">
+                    <input type="checkbox" id="checkBoxDocs_${translit(parentName)}_${translit(doc.Name)}" name="${translit(doc.Name)}" />
+                    <label for="checkBoxDocs_${translit(parentName)}_${translit(doc.Name)}">
+                      <h3 title="${doc.Description}">${doc.Name}</h3>
+                      <span class="checkbox-icon"></span>
+                    </label>
+                  </div>
+                `;
+              }
+              if (doc.TemplateDocs) {
+                generateTemplateDocsHTML(doc.TemplateDocs, `${parentName}${doc.Name}`);
+              }
+            });
+          }
+        }
+
+        generateTemplateDocsHTML(doc.TemplateDocs, translit(doc.Name));
+
+        const docHtml = `
+          <div class="item__props_${translit(doc.Name)}">
+            <div class="custom-checkbox">
+              <input type="checkbox" id="checkBoxArrow_${translit(doc.Name)}" name="checkBoxArrow_${translit(doc.Name)}" />
+              <label for="checkBoxArrow_${translit(doc.Name)}">
+                <h2>${doc.Name}</h2>
+                <span class="arrow-icon"></span>
+              </label>
+              <div class="custom-checkbox">
+                <input type="checkbox" id="checkBox_${translit(doc.Name)}" name="checkBox_${translit(doc.Name)}" />
+                <label for="checkBox_${translit(doc.Name)}">
+                  <span class="checkbox-icon"></span>
+                </label>
+              </div>
+            </div>
+            <div class="${translit(doc.Name)}">
+              ${templateDocsHTML}
+            </div>
+          </div>
+        `;
+        addDocument.innerHTML += docHtml;
+
+        const checkBoxDocInputs = document.querySelectorAll('input[type="checkbox"][id^="checkBox"]');
+        checkBoxDocInputs.forEach(input => {
+          input.addEventListener('change', () => {
+            console.log(`Чекбокс ${input.id} изменился на ${input.checked}`);
+            // Здесь можно добавить дополнительную логику при изменении состояния чекбокса
+          });
+        });
+      });
+    }
   }
 }
-
-loadButton.addEventListener('click', (e) => {
-  processJsonData();
-});
